52faa5a142de04c9db4c4e3485cf1ef3323e9cc8
ErikaHome <ivipech@gmail.com>
2025-08-06 12:48:35 -0600

    Primera leccion

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,46 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!!');
-console.log(40 + 8 + 23 - 15);
-console.log("Love myself");
-alert("I love programming");
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+
+// First Aginment 
+let country = 'Mexico';
+let contintent = 'America';
+let population = 12000000;
+
+console.log(country);
+console.log(contintent);
+console.log(population);
+
+// Data Types
+
+let javascriptisFun = true;
+console.log(typeof javascriptisFun);
+console.log(typeof 23);
+console.log(typeof firstName);
+
+javascriptisFun = 'YES!';
+console.log(typeof javascriptisFun);
+
+// undefined
+/*
+let year;
+console.log('UNDEFINED')
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+// LET CONST VAR
+
+console.log('LET VAR AND CONST');
+
+let age = 31;
+age = 32;
+
+const birthYear = 1993; */
+
 
